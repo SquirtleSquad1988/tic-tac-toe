7755a141a2d220f9d5b086ef4beb5d88b6918521
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-01-28 17:57:43 -0500

    Fix broken commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
-// const ticTacToe = require('../game');
+const ticTacToe = require('../game');
 const handlers = require('./auth/events');
 const poop = require('./server-stuff/events');
 
